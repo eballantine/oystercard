41b51c88991a346acda6e8df24f9fa4f178a7dbf
danroche10 <danielroche10@gmail.com>
2021-08-02 15:02:26 +0100

    step 5 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'has has an initial balance of £0' do
     expect(subject.balance).to eq 0
   end
+
+  describe '#top_up' do
+    it 'top_up method tops up card by correct amount' do
+      starting_balance = subject.balance
+      expect(subject.top_up(10)).to eq starting_balance + 10
+    end
+  end
 end
